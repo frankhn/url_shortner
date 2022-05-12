b15f95f5ed0554cbe444367a763f94856e360e9b
frankhn <harfrank2@gmail.com>
2022-05-12 16:20:14 +0200

    defines url endpoints validations

---
+++ src/resources/controllers/urls/url.validation.ts
@@ -0,0 +1,14 @@
+import { celebrate, Joi } from 'celebrate'
+
+export const createOne = celebrate({
+    body: Joi.object().keys({
+        url: Joi.string().trim().required(),
+        shortcode: Joi.string().trim().alphanum().length(4)
+    })
+})
+
+export const getStats = celebrate({
+    params: Joi.object().keys({
+        shortcode: Joi.string().trim().alphanum()
+    })
+})
